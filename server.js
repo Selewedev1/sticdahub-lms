a72dcf20445efa2ba3418bfa9edd260105408d4b
Selewedev1 <sticdahub@gmail.com>
2026-06-04 17:04:04 +0200

    Fix PORT duplicate error

--- server.js
+++ server.js
@@ -79,8 +79,3 @@ app.listen(PORT, () => {
   console.log("Server running on http://localhost:" + PORT);
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
